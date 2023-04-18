8e4b3f6e6fb0dad1af62621f1494a0563457b3ca
Dogukan Batal <dogukanbatal@gmail.com>
2023-04-18 20:36:59 +0300

    Implement market search

--- src/components/MarketTable/Table.tsx
+++ src/components/MarketTable/Table.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table, InputGroup, Form } from "react-bootstrap";
 import MarketTableRow from "@/components/MarketTable/TableRow";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,6 +8,7 @@ import { Spinner, Alert } from "react-bootstrap";
 import "./MarketTable.scss";
 
 const MarketTable: React.FC = () => {
+  const [search, setSearch] = useState("");
   const dispatch: any = useDispatch();
 
   useEffect(() => {
@@ -18,8 +19,6 @@ const MarketTable: React.FC = () => {
     (state: { market: IMarketState }) => state.market.data
   );
 
-  console.log(market);
-
   const isLoading = useSelector(
     (state: { market: IMarketState }) => state.market.loading
   );
@@ -28,47 +27,65 @@ const MarketTable: React.FC = () => {
     (state: { market: IMarketState }) => state.market.error
   );
 
-  if (isLoading) {
-    return (
-      <div className="my-4">
-        <Spinner animation="border" />
-      </div>
-    );
-  }
+  const handleSearch = (e: any) => {
+    let value = e.target.value;
+    setSearch(value.replace(/\s+/g, ""));
+  };
 
-  if (error) {
-    return (
-      <div className="my-4">
-        <Alert variant="warning">An error occured to fetch market data</Alert>
-      </div>
-    );
-  }
+  const filteredMarket = !search
+    ? market
+    : market?.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search.toLowerCase()) ||
+          item.symbol.toLowerCase().includes(search.toLowerCase())
+      );
 
   return (
     <div className="section-gap">
       <div className="section-head">
         <h4 className="section-title">Cryptocurrency Prices</h4>
         <InputGroup className="marketTable-search">
-          <Form.Control type="text" placeholder="Search a coin" />
+          <Form.Control
+            type="text"
+            placeholder="Search a coin"
+            onChange={handleSearch}
+          />
         </InputGroup>
       </div>
-      <Table hover responsive className=" marketTable">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Coin</th>
-            <th className="text-end">Price</th>
-            <th className="text-end">24h</th>
-            <th className="text-end">24h Vol</th>
-            <th className="text-end">Market Cap</th>
-          </tr>
-        </thead>
-        <tbody>
-          {market?.map((item: IMarketCoin, index: number) => (
-            <MarketTableRow key={index} item={item} />
-          ))}
-        </tbody>
-      </Table>
+
+      {isLoading && (
+        <div className="my-4">
+          <Spinner animation="border" />
+        </div>
+      )}
+
+      {error && (
+        <div className="my-4">
+          <Alert variant="warning">An error occured to fetch market data</Alert>
+        </div>
+      )}
+
+      {filteredMarket && filteredMarket.length ? (
+        <Table hover responsive className=" marketTable">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Coin</th>
+              <th className="text-end">Price</th>
+              <th className="text-end">24h</th>
+              <th className="text-end">24h Vol</th>
+              <th className="text-end">Market Cap</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredMarket?.map((item: IMarketCoin, index: number) => (
+              <MarketTableRow key={index} item={item} />
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <Alert variant="warning">It was not found</Alert>
+      )}
     </div>
   );
 };
